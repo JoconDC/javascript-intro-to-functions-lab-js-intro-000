31fef4cdbec814c8b40415a4fbb8b7c0d8cf4d72
Jojo Concepcion <hsconcepcion@gmail.com>
2020-04-17 01:32:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+
+function sayHiToGrandma(string) {
+
+  if (uppercase.toUpperCase() === uppercase) {
+  return "YES INDEED!";
+}
+  else if (lowercase.toLowerCase()= lowercase) {
+    return "I cant hear you";
+  }
+
+  else if (mixedCase.toLowerCase() === mixedCase) {
+    return "I love you, too."
+}  else if (mixedCase.toUpperCase() === mixedCase) {
+     return "I love you, too.";
+}
+} 
