122293d92fc3946da27bc1c1acd5417e68d510f0
Jojo Concepcion <hsconcepcion@gmail.com>
2020-04-17 02:45:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,15 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-   if (string === string.toLowerCase()) return "I can't hear you!";
-   else if (string === "I love you, Grandma.") return "I love you, too.";
-   else if (string === string.toUpperCase()) return "YES INDEED!";
- }
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
 }
   sayHiToGrandma("hello");
  sayHiToGrandma("HELLO");
