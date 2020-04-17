ff2239be49873573b9c228663a15aa3919554e81
Jojo Concepcion <hsconcepcion@gmail.com>
2020-04-17 02:40:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,22 +17,15 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
 
-  var uppercase = "HELLO!";
-  if (uppercase.toUpperCase() === uppercase) {
-  return "YES INDEED!";
+  function sayHiToGrandma(string){
+   if (string === string.toLowerCase()) return "I can't hear you!";
+   else if (string === "I love you, Grandma.") return "I love you, too.";
+   else if (string === string.toUpperCase()) return "YES INDEED!";
+ }
 }
-  var lowercase = "hello!";
-  else if (lowercase.toLowerCase() === lowercase) {
-    return "I can't hear you!";
-  }
+  sayHiToGrandma("hello");
+ sayHiToGrandma("HELLO");
+ sayHiToGrandma("I love you, Grandma.");
 
-  else if (mixedCase.toLowerCase() === mixedCase) {
-    return "I love you, too."
-}  else if (mixedCase.toUpperCase() === mixedCase) {
-     return "I love you, too.";
-}
-}
 
-var uppercase = "HELLO!";
-var lowercase = "hello";
-var mixedCase = "I love you, Grandma.";
+
